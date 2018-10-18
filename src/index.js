f92f07e5393c85dcb922038117c053a4c3e816fa
var <varenik16@gmail.com>
2018-10-18 13:04:34 +0400

    fix eport

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import {all} from 'redux-saga/effects';
 import crudReducers from './redux/reducer'
 import crudSagas from './redux/saga'
-import Action from './components/crud/action'
-import CreateModel from './components/crud/createModel'
 import CrudFull from './components/crud/crudFull'
 import crudView from './components/crud/crudView'
-import DataRenderer from './components/crud/dataRenderer'
-import FilterDeopDown from './components/crud/filterDropdown'
-import FilterRenderer from './components/crud/filterRenderer'
-
-module.exports = require('./components/crud/');
-
 
 export const reducer = {
 	...crudReducers,
