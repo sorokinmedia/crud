1dfae92d4c3b31b41c9f06deb1bc355b42b6ec03
var <varenik16@gmail.com>
2018-10-18 20:31:38 +0400

    reducers only

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 //import 'antd/dist/antd.css';
 import crudReducers from './redux/reducer'
-import crudSagas from './redux/saga'
-import crudActions from './redux/actions'
+//import crudSagas from './redux/saga'
+//import crudActions from './redux/actions'
 /*import crudFull from './components/crud/crudFull'
 import crudView from './components/crud/crudView'*/
 
 export const reducer = crudReducers;
-export const saga = crudSagas;
-export const actions = crudActions;
+//export const saga = crudSagas;
+//export const actions = crudActions;
 //export const CrudView = crudView;
 //export const CrudFull = crudFull;
