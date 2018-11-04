2f547c7ddb7214ab6524e3c346f56e89b2a5d870
ibm <magarampage@mail.ru>
2018-11-04 22:39:12 +0300

    [fix] style

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import crudFull from './components/crud/crudFull'
+import crudView from './components/crud/crudView'
+import crudActions from './redux/actions'
 //import 'antd/dist/antd.css';
 import crudReducers from './redux/reducer'
 import crudSagas from './redux/saga'
-import crudActions from './redux/actions'
-import crudFull from './components/crud/crudFull'
-import crudView from './components/crud/crudView'
-import regeneratorRuntime from 'regenerator-runtime'
+import './style.css'
 
 export const reducer = crudReducers;
 export const saga = crudSagas;
 export const actions = crudActions;
 export const CrudView = crudView;
 export const CrudFull = crudFull;
-import './style.css'
