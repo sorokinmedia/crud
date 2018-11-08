1baeaea5473167b4fae469c263af18b1e0aff712
var <varenik16@gmail.com>
2018-11-08 13:53:26 +0400

    update antd

--- src/helpers/renderField.js
+++ src/helpers/renderField.js
@@ -1,88 +1,89 @@
 import React from 'react';
-import {Input, Form, Checkbox, Select, DatePicker} from 'antd'
+import { Input, Form, Checkbox, Select, DatePicker } from 'antd'
 import moment from 'moment'
 import 'moment/locale/ru'
 import locale from 'antd/lib/date-picker/locale/ru_RU'
 
-const {Option : SelectOption} = Select;
+const { Option: SelectOption } = Select;
 const Search = Input.Search;
 
 export const renderField = ({
-    input,
-    label,
-    type,
-    options,
+	input,
+	label,
+	type,
+	options,
 	mode,
-    min,
-    max,
-    step,
-    meta: { touched, error, warning },
-    validating,
-    placeholder,
-    rows,
+	min,
+	max,
+	step,
+	meta: { touched, error, warning },
+	validating,
+	placeholder,
+	rows,
 	size,
-    layout,
+	layout,
 	onPressEnter,
-    defaultValue,
-    addonAfter,
-    enterButton
-}) => <Form.Item
+	defaultValue,
+	addonAfter,
+	enterButton
+}) => (<Form.Item
 	hasFeedback
 	{...layout}
 	label={label}
 	validateStatus={error && touched ? 'error'
 		: warning && touched ? 'warning'
 			: validating ? 'validating'
-	: ''}
+				: ''}
 	help={touched && error ? error : ''}
 >
-	{(() => {switch (type) {
+	{(() => {
+		switch (type) {
 		case 'select':
-			return <Select
+			return (<Select
 				{...input}
 				value={input.value || []}
 				mode={mode}
-				style={{width: '100%'}}
+				style={{ width: '100%' }}
 				placeholder={placeholder}
 			>
-				{options.map(elem => <SelectOption value={elem.id} key={elem.id}>
+				{options.map(elem => (<SelectOption value={elem.id} key={elem.id}>
 					{elem.name}
-				</SelectOption>)}
-			</Select>;
+                </SelectOption>))}
+           </Select>);
 		case 'textarea':
-			return <Input.TextArea
+			return (<Input.TextArea
 				{...input}
 				placeholder={placeholder}
 				type={type}
 				className="form-control"
 				rows={rows}
-			/>;
+			/>);
 		case 'checkbox':
-			return <Checkbox {...input}>
+			return (<Checkbox {...input}>
 				{placeholder}
-			</Checkbox>;
+			</Checkbox>);
 		case 'search':
-			return <Search
+			return (<Search
 				onPressEnter={onPressEnter}
 				{...input}
 				value={input.value || defaultValue}
 				placeholder={placeholder}
 				enterButton={enterButton}
-			/>;
+			/>);
 		case 'date':
 			console.log(input)
-			return <DatePicker
-				style={{width: '100%'}}
+			return (<DatePicker
+				style={{ width: '100%' }}
 				onPressEnter={onPressEnter}
 				{...input}
 				value={input.value ? moment(input.value, 'DD/MM/YYYY') : null}
 				placeholder={placeholder}
-				//onChange={(value) => console.log(value)}
-				format={'DD/MM/YYYY'}
+				// onChange={(value) => console.log(value)}
+				format="DD/MM/YYYY"
 				locale={locale}
-			/>;
+			/>);
 		default:
-			return <Input
+			return (<Input
 				onPressEnter={onPressEnter}
 				{...input}
 				value={input.value || defaultValue}
@@ -94,6 +95,7 @@ export const renderField = ({
 				step={step}
 				size={size}
 				addonAfter={addonAfter}
-			/>;
-	}})()}
-</Form.Item>;
+			/>);
+		}
+	})()}
+</Form.Item>);
