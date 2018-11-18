7b0679ef2cbfc567deac67c887cc7894ab427620
var <varenik16@gmail.com>
2018-11-19 00:31:17 +0400

    add Loader theme

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,34 +1,58 @@
 const actions = {
-    FETCH_CRUD_FILTER_VALUES: 'FETCH_CRUD_FILTER_VALUES',
-    FETCH_CRUD_MODELS: 'FETCH_CRUD_MODELS',
-    SET_CRUD_ACTIONS_FUNC:  'SET_CRUD_ACTIONS_FUNC',
-    SET_CRUD_PARAMS:  'SET_CRUD_PARAMS',
+	FETCH_CRUD_FILTER_VALUES: 'FETCH_CRUD_FILTER_VALUES',
+	FETCH_CRUD_MODELS: 'FETCH_CRUD_MODELS',
+	SET_CRUD_ACTIONS_FUNC: 'SET_CRUD_ACTIONS_FUNC',
+	SET_CRUD_PARAMS: 'SET_CRUD_PARAMS',
 
-    DELETE_MODEL: 'DELETE_MODEL',
-    RESTORE_MODEL: 'RESTORE_MODEL',
-    CREATE_MODEL: 'CREATE_MODEL',
-    CHANGE_MODEL: 'CHANGE_MODEL',
-    TOGGLE_CREATE_MODEL_MODAL: 'TOGGLE_CREATE_MODEL_MODAL',
-    SET_MODEL_MODAL_FORM: 'SET_MODEL_MODAL_FORM',
-    toggleCreateModelModal: () => ({type: actions.TOGGLE_CREATE_MODEL_MODAL}),
+	DELETE_MODEL: 'DELETE_MODEL',
+	RESTORE_MODEL: 'RESTORE_MODEL',
+	CHANGE_MODEL: 'CHANGE_MODEL',
+	CREATE_MODEL: 'CREATE_MODEL',
+	TOGGLE_CREATE_MODEL_MODAL: 'TOGGLE_CREATE_MODEL_MODAL',
+	SET_MODEL_MODAL_FORM: 'SET_MODEL_MODAL_FORM',
+	toggleCreateModelModal: () => ({ type: actions.TOGGLE_CREATE_MODEL_MODAL }),
 
-    deleteModel: (id, url, modelName) => ({type: actions.DELETE_MODEL, payload: {id, url, modelName} }),
-    restoreModel: (id, url, modelName) => ({type: actions.RESTORE_MODEL, payload: {id, url, modelName} }),
-    createModel: (form, url, modelName) => ({type: actions.CREATE_MODEL, payload: {form, url, modelName} }),
-    changeModel: (form, action, modelName) => ({type: actions.CHANGE_MODEL, payload: {form, action, modelName} }),
+	deleteModel: (id, url, modelName) => ({
+		type: actions.DELETE_MODEL,
+		payload: {
+			id, url, modelName
+		}
+	}),
+	restoreModel: (id, url, modelName) => ({
+		type: actions.RESTORE_MODEL,
+		payload: {
+			id, url, modelName
+		}
+	}),
+	createModel: (form, url, modelName) => ({
+		type: actions.CREATE_MODEL,
+		payload: {
+			form, url, modelName
+		}
+	}),
+	changeModel: (form, action, modelName) => ({
+		type: actions.CHANGE_MODEL,
+		payload: {
+			form, action, modelName
+		}
+	}),
 
-    setModelModalForm: (modalType, initialValues, action) => ({
-        type: actions.SET_MODEL_MODAL_FORM,
-        payload: {modalType, initialValues, action}
-    }),
+	setModelModalForm: (modalType, initialValues, action) => ({
+		type: actions.SET_MODEL_MODAL_FORM,
+		payload: {
+			modalType, initialValues, action
+		}
+	}),
 
-    setCrudActionsFunc: (func, modelName) => ({type: actions.SET_CRUD_ACTIONS_FUNC, payload: {func, modelName}}),
-    setCrudParams: (params) => ({type: actions.SET_CRUD_PARAMS, payload: params}),
-    fetchCrudModels: (params, filters) => ({type: actions.FETCH_CRUD_MODELS, payload: {params, filters} }),
-    fetchCrudFilterValues: (modelName, filter, query) => ({
-        type: actions.FETCH_CRUD_FILTER_VALUES,
-        payload: {query, filter, modelName}
-    }),
+	setCrudActionsFunc: (func, modelName) => ({ type: actions.SET_CRUD_ACTIONS_FUNC, payload: { func, modelName } }),
+	setCrudParams: params => ({ type: actions.SET_CRUD_PARAMS, payload: params }),
+	fetchCrudModels: (params, filters) => ({ type: actions.FETCH_CRUD_MODELS, payload: { params, filters } }),
+	fetchCrudFilterValues: (modelName, filter, query) => ({
+		type: actions.FETCH_CRUD_FILTER_VALUES,
+		payload: {
+			query, filter, modelName
+		}
+	}),
 };
 
 export default actions;
