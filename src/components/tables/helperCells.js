602fe4c6d412d536d9ac5e2873d38ef4cd2cfb3a
var <varenik16@gmail.com>
2018-11-22 20:50:57 +0400

    fix

--- src/components/tables/helperCells.js
+++ src/components/tables/helperCells.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Action from '../crud/action'
 import moment from 'moment'
+import { Icon } from 'antd'
 
 const DateCell = data => <p>{moment(data).format('DD.MM.YYYY')}</p>;
 const TextCell = text => <span>{text}</span>;
-const BooleanCell = value => <span>{value ? 'Да' : 'Нет'}</span>;
+const BooleanCell = value => <span>{value ?
+	<Icon type={'check'} style={{color: 'green'}} />
+	: <Icon type={'close'} style={{color: 'red'}} />
+}</span>;
 const ArrTextCell = arr => arr.map(elem => <p>{elem}</p>);
 const ActionsCell = (row, modelName, iconTheme) => row.actions.map(action =>
 	<Action data={action} row={row} key={action.id} modelName={modelName} iconTheme={iconTheme} />
